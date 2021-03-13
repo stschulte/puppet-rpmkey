349e516e300d2b85c83eaaeadfe36152f264baaf
Stefan Schulte <stschulte@posteo.de>
2021-03-13 16:57:48 +0100

    maint: Adapt code to follow latest styleguide
    Update our puppet type in order to follow the latest styleguide and make
    rubocop happy

--- lib/puppet/provider/rpmkey/rpm.rb
+++ lib/puppet/provider/rpmkey/rpm.rb
@@ -1,20 +1,19 @@
 Puppet::Type.type(:rpmkey).provide(:rpm) do
-
-  commands :rpm => 'rpm'
+  commands rpm: 'rpm'
 
   def self.instances
     keys = []
 
     begin
-      rpm_query = rpm('-q','gpg-pubkey')
+      rpm_query = rpm('-q', 'gpg-pubkey')
     rescue Puppet::ExecutionFailure
       return []
     end
 
     rpm_query.each_line do |line|
       line.chomp!
-      if match = /^gpg-pubkey-([0-9a-f]*)-[0-9a-f]*/.match(line)
-        keys << new(:name => match.captures[0].upcase, :ensure => :present)
+      if (match = %r{^gpg-pubkey-([0-9a-f]*)-[0-9a-f]*}.match(line))
+        keys << new(name: match.captures[0].upcase, ensure: :present)
       else
         warning "Unexpected rpm output #{line.inspect}. Ignoring this line."
       end
@@ -24,14 +23,14 @@ Puppet::Type.type(:rpmkey).provide(:rpm) do
 
   def self.prefetch(resources)
     instances.each do |prov|
-      if resource = resources[prov.name]
+      if (resource = resources[prov.name])
         resource.provider = prov
       end
     end
   end
 
   def create
-    raise Puppet::Error, "Cannot add key without a source" unless @resource[:source]
+    raise Puppet::Error, 'Cannot add key without a source' unless @resource[:source]
     rpm('--import', @resource[:source])
   end
 
@@ -42,5 +41,4 @@ Puppet::Type.type(:rpmkey).provide(:rpm) do
   def destroy
     rpm('-e', '--allmatches', "gpg-pubkey-#{@resource[:name].downcase}")
   end
-
 end
