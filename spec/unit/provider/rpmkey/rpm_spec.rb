cccbf90e4aceb17db119b88972e43786965d4775
Stefan Schulte <stschulte@posteo.de>
2021-03-13 15:18:24 +0100

    maint: Run tests against recent puppet versions
    In order to be able to test our code and verify it runs with recent
    versions of puppet make sure we adapt it with `pdk convert`.
    
    We also make sure that our rspec tests are using the preferred
    rspec-mock framework and rubocop is happy with our tests.

--- spec/unit/provider/rpmkey/rpm_spec.rb
+++ spec/unit/provider/rpmkey/rpm_spec.rb
@@ -3,91 +3,103 @@
 require 'spec_helper'
 
 describe Puppet::Type.type(:rpmkey).provider(:rpm) do
-
   before :each do
-    Puppet::Type.type(:rpmkey).stubs(:defaultprovider).returns described_class
+    allow(Puppet::Type.type(:rpmkey)).to receive(:defaultprovider).and_return(described_class)
   end
 
-  describe ".instances" do
-    it "should have an instances method" do
-      expect(described_class).to respond_to(:instances)
+  describe 'instances' do
+    it 'has an instances method' do
+      expect(described_class).to respond_to :instances
+    end
+  end
+
+  describe 'prefetch' do
+    it 'has a prefetch method' do
+      expect(described_class).to respond_to :prefetch
+    end
+  end
+
+  context 'self.instances multiple keys' do
+    before :each do
+      allow(described_class).to receive(:rpm).with('-q', 'gpg-pubkey').and_return(File.read(my_fixture('rpm_q')))
     end
 
-    it "should get installed rpm keys by running rpm -q" do
-      described_class.expects(:rpm).with('-q', 'gpg-pubkey').returns File.read(my_fixture('rpm_q'))
-      expect(described_class.instances.map(&:name)).to eq([
-        'DB42A60E',
-        '4F2A6FD2',
-        '23A254D4'
-      ])
+    it 'returns all keys' do
+      expect(described_class.instances.map(&:name)).to eq(['DB42A60E', '4F2A6FD2', '23A254D4'])
+    end
+  end
+
+  context 'self.instances no key installed' do
+    before :each do
+      allow(described_class).to receive(:rpm).with('-q', 'gpg-pubkey').and_raise Puppet::ExecutionFailure, 'package gpg-pubkey is not installed'
     end
 
-    it "should return an empty list if no key is installed" do
-      described_class.expects(:rpm).with('-q', 'gpg-pubkey').raises Puppet::ExecutionFailure, 'package gpg-pubkey is not installed'
+    it 'returns and empty list' do
       expect(described_class.instances).to be_empty
     end
+  end
 
-    it "should warn and ignore unexpected output" do
-      described_class.expects(:rpm).with('-q', 'gpg-pubkey').returns File.read(my_fixture('rpm_q_unexpected'))
-      described_class.expects(:warning).with('Unexpected rpm output "gpg-pubkey-4f2a6fd3". Ignoring this line.')
-      expect(described_class.instances.map(&:name)).to eq([
-        'DB42A60E',
-        '4F2A6FD2',
-        '23A254D4'
-      ])
+  context 'self.instances returns unexpected lines' do
+    before :each do
+      allow(described_class).to receive(:rpm).with('-q', 'gpg-pubkey').and_return(File.read(my_fixture('rpm_q_unexpected')))
+    end
+
+    it 'warns and ignore unexpected output' do
+      expect(described_class).to receive(:warning).with('Unexpected rpm output "gpg-pubkey-4f2a6fd3". Ignoring this line.')
+      expect(described_class.instances.map(&:name)).to eq(['DB42A60E', '4F2A6FD2', '23A254D4'])
     end
   end
 
-  describe "#exists?" do
-    it "should return true if the resource is present" do
-      provider = described_class.new(:name => 'DB42A60E', :ensure => :present)
+  describe '#exists?' do
+    it 'returns true if the resource is present' do
+      provider = described_class.new(name: 'DB42A60E', ensure: :present)
       expect(provider).to be_exists
     end
 
-    it "should return false if the resource is absent" do
-      provider = described_class.new(:name => 'DB42A60E', :ensure => :absent)
+    it 'returns false if the resource is absent' do
+      provider = described_class.new(name: 'DB42A60E', ensure: :absent)
       expect(provider).not_to be_exists
     end
   end
 
-  describe "#create" do
-    it "should import the key" do
+  describe '#create' do
+    it 'imports the key' do
       provider = described_class.new(Puppet::Type.type(:rpmkey).new(
-        :name   => 'DB42A60E',
-        :source => 'http://example.com/foo',
-        :ensure => :present
+        name: 'DB42A60E',
+        source: 'http://example.com/foo',
+        ensure: :present,
       ))
-      provider.expects(:rpm).with('--import', 'http://example.com/foo')
+      expect(provider).to receive(:rpm).with('--import', 'http://example.com/foo')
       provider.create
     end
 
-    it "should raise an error if no source is specified" do
+    it 'raises an error if no source is specified' do
       provider = described_class.new(Puppet::Type.type(:rpmkey).new(
-        :name   => 'DB42A60E',
-        :ensure => :present
+        name: 'DB42A60E',
+        ensure: :present,
       ))
-      expect { provider.create }.to raise_error(Puppet::Error, /Cannot add key without a source/)
+      expect { provider.create }.to raise_error(Puppet::Error, %r{Cannot add key without a source})
     end
   end
 
-  describe "#destroy" do
-    it "should remove the key" do
+  describe '#destroy' do
+    it 'removes the key' do
       provider = described_class.new(Puppet::Type.type(:rpmkey).new(
-        :name   => 'DB42A60E',
-        :source => 'http://example.com/foo',
-        :ensure => :absent
+        name: 'DB42A60E',
+        source: 'http://example.com/foo',
+        ensure: :absent,
       ))
-      provider.expects(:rpm).with('-e', '--allmatches', 'gpg-pubkey-db42a60e')
+      expect(provider).to receive(:rpm).with('-e', '--allmatches', 'gpg-pubkey-db42a60e')
       provider.destroy
     end
 
-    it "should not complain about a missing source" do
+    it 'does not complain about a missing source' do
       provider = described_class.new(Puppet::Type.type(:rpmkey).new(
-        :name   => 'DB42A60E',
-        :ensure => :absent
+        name: 'DB42A60E',
+        ensure: :absent,
       ))
-      provider.expects(:rpm).with('-e', '--allmatches', 'gpg-pubkey-db42a60e')
-      expect { provider.destroy }.to_not raise_error
+      expect(provider).to receive(:rpm).with('-e', '--allmatches', 'gpg-pubkey-db42a60e')
+      expect { provider.destroy }.not_to raise_error
     end
   end
 end
